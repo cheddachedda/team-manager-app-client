3fdb944eeb4a447def4426768f74fe89b16d007e
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-16 18:55:10 +1100

    Refactor Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,17 @@ import '../css/App.css';
 import { Link } from 'react-router-dom';
 
 const Nav = (props) => {
+  // Put new links in here:
+  const links = [
+    { path: '/', label: 'Home' },
+    { path: '/user', label: 'User' },
+    { path: '/signin', label: 'Sign In' },
+    { path: '/signup', label: 'Sign Up' },
+    { path: '/ladder', label: 'Ladder' },
+    { path: '/games', label: 'Games' },
+    { path: '/catgame', label: 'Meow-Meow-Meow' },
+  ];
+
   const logOut = () => {
     window.location.reload(false);
     localStorage.clear();
@@ -10,7 +21,7 @@ const Nav = (props) => {
 
   return (
     <nav>
-      { props.links.map((link) => <Link to={ link.path }>{ link.label }</Link>) }
+      { links.map((link) => <Link to={ link.path }>{ link.label }</Link>) }
 
       <Link to={ '/' } onClick={ logOut }>
         Log out

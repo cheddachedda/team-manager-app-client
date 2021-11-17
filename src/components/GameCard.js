2244dd82fd24fb31aebf1560e14a11af9b669486
Stephen <stephen.sant123@gmail.com>
2021-11-17 12:30:26 +1100

    Add .selected and .current css

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,12 +1,9 @@
 const GameCard = (props) => {
   const game = props.game;
   return (
-    // TODO: Build this component nicer
-    <div>
-      <div className="game-card">
-        <div><b>{ game.home }</b></div><div> {/* game.home_score */}01 v 23{ /*game.away_score*/ } </div><div><b>{ game.away }</b></div><div/>
-        <div>Round: { game.round }</div><div> | </div><div>Venue: { game.venue }</div>
-      </div>
+    <div className="game-card">
+      <div><b>{ game.home }</b></div><div> { game.home_score } v { game.away_score } </div><div><b>{ game.away }</b></div><div/>
+      <div>Round: { game.round }</div><div> | </div><div>Venue: { game.venue }</div>
     </div>
   )
 };

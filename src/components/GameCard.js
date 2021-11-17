88a70534db2936cdf6f7ae84c72027c4e3b7b1fa
Stephen <stephen.sant123@gmail.com>
2021-11-17 11:37:23 +1100

    Add scores to game card

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -4,7 +4,7 @@ const GameCard = (props) => {
     // TODO: Build this component nicer
     <div>
       <div className="game-card">
-        <div>{ game.home }</div><div> v </div><div>{ game.away }</div><div/>
+        <div><b>{ game.home }</b></div><div> {/* game.home_score */}01 v 23{ /*game.away_score*/ } </div><div><b>{ game.away }</b></div><div/>
         <div>Round: { game.round }</div><div> | </div><div>Venue: { game.venue }</div>
       </div>
     </div>
